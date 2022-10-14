30a605bb833422601e1d85f90b559e0b30f8ee54
LuaMoraUtad <114484033+LuaMoraUtad@users.noreply.github.com>
2022-10-14 17:02:54 +0200

    Tablas creadas para mostrar a los usuarios

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ const showParticipants = (data) => {
     let arrayAuxiliar = [];
 
     data.forEach(element => {
-        arrayAuxiliar.push(element.name);
+        arrayAuxiliar.push(element);
     });
 
     //$("#users").text("Participants: " + arrayAuxiliar);
-    $("#users").html("<strong>Participants: </strong>" + arrayAuxiliar);
+    $("#users").html("<strong>Participants: </strong></br>");
+    arrayAuxiliar.forEach(element => {
+        $("#users").append("<div class='persona' >" +
+                            "<label class='dato'> Nombre:" + element.name + "</label></br>" +
+                            "<label class='subdato'> Correo: " + element.email + "</label></br>" +
+                            "<label class='subdato'> Ciudad: " + element.address.street + "</label></br>" +
+                            "<label class='subdato'> Telefono: " + element.phone + "</label></br>" +
+                            "<label class='subdato'> Website: " + element.website + "</label></br>" +
+                            "</div></br>");
+    });
+    
 }
 
 const getWinner = (data) => {
     const aleat = Math.floor(Math.random() * data.length);
 
-    $("#selected").html("<strong>Seleccionado: </strong>" + data[aleat].name);
+    $("#selected").html("<strong>Seleccionado: " + 
+    
+                    "<label class='ganador'>" + data[aleat].name + "</label></strong>");
 }
 
 $(document).ready(function(){
